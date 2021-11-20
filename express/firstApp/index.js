dbbfc223cb9ed2d6afd7d4eee649a6036d51205f
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-20 18:56:48 +0100

    learnt basics of request and response objects in express

--- express/firstApp/index.js
+++ express/firstApp/index.js
@@ -3,9 +3,11 @@ const app = express();
 // console.dir(app);
 
 // This always runs anytime a request is send specifically to the listening port
-app.use(() => {
+app.use((req, res) => {
   console.log("A new request has just been sent!");
+  res.send("Hello bastard! I got your request. Take it! Your father!");
 });
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
