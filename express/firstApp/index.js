aed5497a5d7ba848e6a4c2692fd91d3dd6d9f71d
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-20 19:15:20 +0100

    learnt the basics of express routing

--- express/firstApp/index.js
+++ express/firstApp/index.js
@@ -3,10 +3,32 @@ const app = express();
 // console.dir(app);
 
 // This always runs anytime a request is send specifically to the listening port
-app.use((req, res) => {
-  console.log("A new request has just been sent!");
-  res.send("Hello bastard! I got your request. Take it! Your father!");
+// app.use((req, res) => {
+//   console.log("A new request has just been sent!");
+//   res.send("Hello bastard! I got your request. Take it! Your father!");
+// });
+
+// If the below is set at the beginning of the file, the other get request routes
+// would not be located
+app.get("*", (req, res) => {
+  res.send("This is not the path");
 });
+
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW!!!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("Whoof!!");
+});
+
+// app.get("*", (req, res) => {
+//   res.send("This is not the path");
+// });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
