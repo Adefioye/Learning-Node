16355bfa96edc3edcf76a00181d0894fc2ff3616
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-23 04:58:00 +0100

    finished using uuid package to perform POST method

--- resful-routes/restDemo/index.js
+++ resful-routes/restDemo/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 
 
 app.use(express.json());  // for parsing JSON payload
@@ -9,23 +10,27 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
 const comments = [
-    {
-        username: "Todd",
-        comment: "lol that is so funny"
-    },
-    {
-        username: "Skyler",
-        comment: "I like to go birdwatching with my dog"
-    },
-    {
-        username: "Sk8erBoi",
-        comment: "Plz delete your account, Todd"
-    },
-    {
-        username: "onlysayswoof",
-        comment: "woof woof woof"
-    }
-]
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "lol that is so funny",
+  },
+  {
+    id: uuid(),
+    username: "Skyler",
+    comment: "I like to go birdwatching with my dog",
+  },
+  {
+    id: uuid(),
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    id: uuid(),
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+];
 app.get("/comments", (req, res) => {
     res.render("comments/index", { comments })
 })
@@ -36,8 +41,15 @@ app.get("/comments/new", (req, res) => {
 
 app.post("/comments", (req, res) => {
     const {username, comment} = req.body
-    comments.push({username, comment})
-    res.send("It worked!")
+    comments.push({ username, comment, id: uuid() });
+    res.redirect("/comments")
+})
+
+// Showing one comment using path parameters
+app.get("/comments/:id", (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render("comments/show", {comment})
 })
 
 
