71b33176664583fc78ac0b4bb7addac1ddc40ebb
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-28 08:32:42 +0100

    added filtering feature to the API

--- mongoose-express/index.js
+++ mongoose-express/index.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const methodOverride = require("method-override");
+
+const categories = ["fruit", "vegetable", "dairy"];
 
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/farmStand");
@@ -19,17 +22,47 @@ main()
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.render("products/index", { products });
+  const { category } = req.query;
+  console.log(req.query);
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
 });
 
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const updateProduct = await Product.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.redirect(`/products/${updateProduct._id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
 });
 
 app.get("/product/new", (req, res) => {
