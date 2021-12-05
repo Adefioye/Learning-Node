7cebeaf0ee4a25950979a3adf9bba95a7e978da7
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-12-05 17:34:22 +0100

    learnt basics of express session

---
+++ session-demo/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "thisisnotagoodsecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/viewcount", (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`YOU HAVE VIEWED THIS PAGE ${req.session.count} TIMES`);
+});
+
+app.listen(3000, () => {
+  console.log("APP LISTENING ON PORT 3000");
+});
